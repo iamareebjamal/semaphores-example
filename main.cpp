c63bb24f222f90ad54187096afed09ee8037883d
iamareebjamal <jamal.areeb@gmail.com>
2017-02-15 02:05:57 +0530

    feat: Add chopsticks vector and test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <vector>
 
 #include "semaphore.h"
 
@@ -12,26 +13,43 @@ void print_data(int i) {
     }
 }
 
+template <typename T>
+void clean_memory(T objects) {
+    typename T::const_iterator it;
+
+    for(it = objects.begin(); it != objects.end(); ++it)
+        delete *it;
+
+}
+
 int main() {
+    vector<semaphore*> chopsticks;
 
     try {
 
-        semaphore sem;
-
-        int i;
-        for (i = 0; i < 5; ++i) {
-            int pid = fork();
-            if (pid == 0)
-                break;
-        }
-
-        sem.wait();
-        print_data(i);
-        sem.post();
+        for (int i = 0; i < 6; ++i)
+            chopsticks.push_back(new semaphore);
 
     } catch (string error) {
         cout << error << endl;
+
+        return 0;
     }
+
+
+    int num;
+    for (num = 0; num < 5; ++num) {
+        int pid = fork();
+        if (pid == 0)
+            break;
+    }
+
+    chopsticks[0]->wait();
+    print_data(num);
+    chopsticks[0]->post();
+
+    clean_memory(chopsticks);
+
 
     return 0;
 }
